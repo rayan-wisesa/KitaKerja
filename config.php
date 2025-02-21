d4334973ff08f037dc19f1ad87cfb1f0875117b0
isyamarshabindarputra <isyabindarp@gmail.com>
2025-02-21 22:43:52 +0700

    yambungin data base

--- config.php
+++ config.php
@@ -1,1 +1,13 @@
-<p>vh</p>
+<?php
+// Database Connection
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "kita_kerja";
+
+$conn = mysqli_connect($host, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
