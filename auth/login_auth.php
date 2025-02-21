7b0786970cff14f200b890712740329966a4930e
isyamarshabindarputra <isyabindarp@gmail.com>
2025-02-21 22:48:26 +0700

    ya itulah

---
+++ auth/login_auth.php
@@ -0,0 +1,42 @@
+<?php
+session_start();
+require_once("../config.php");
+if ($_SERVER["REQUEST_METHOD"]== "POST") {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    $sql ="SELECT * FROM users WHERE username='$username'";
+    $result = $conn->query($sql);
+
+    if($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+
+        if (password_verify($password, $row["password"])){
+            $_SESSION["username"] = $username;
+            $_SESSION["name"] = $row["name"];
+            $_SESSION["role"] = $row["role"];
+            $_SESSION["user_id"] = $row["user_id"];
+            $_SESSION['notification'] = [
+                'type' => 'primary',
+                'message' =>'selamat datang kembali!'
+            ];
+            header('location: ../dashboard.php');
+            exit();
+        } else {
+                $_SESSION['notification'] = [
+                    'type' => 'danger',
+                    'message' => 'username atau password salah!'
+            ];
+        }
+        }else{
+            $_SESSION['notification'] = [
+                'type' => 'danger',
+                    'message' => 'username atau password salah!'
+            ];
+        }
+        header('Location: login.php');
+        exit();
+    }
+    $conn->close();
+
+?>
