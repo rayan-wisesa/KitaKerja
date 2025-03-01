378b18dfb104891898e75f4bdb247893bc8d38c3
isyamarshabindarputra <isyabindarp@gmail.com>
2025-03-01 15:50:19 +0700

    Membuat Koneksi PHP dengan Basis Data Mysql

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+// Database Connection
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "KitaKerja";
+
+$conn = mysqli_connect($host, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
