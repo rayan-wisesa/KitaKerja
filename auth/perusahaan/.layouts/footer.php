19d559b151e1c62d3360c6a7122583529b29bbfe
sleepy-head-r <rayanwisesaazhari@gmail.com>
2025-03-05 23:46:29 +0700

    Perbaiki script js path tidak ditemukan

--- auth/perusahaan/.layouts/footer.php
+++ auth/perusahaan/.layouts/footer.php
@@ -4,9 +4,9 @@
     <!-- / Content -->
     <!-- Core JS -->
     <!-- build:js ../assets/vendor/js/core.js -->
-    <script src="../assets/vendor/libs/jquery/jquery.js"></script>
-    <script src="../assets/vendor/js/bootstrap.bundle.min.js"></script>
+    <script src="../../assets/vendor/libs/jquery/jquery.js"></script>
+    <script src="../../assets/vendor/js/bootstrap.bundle.min.js"></script>
     <!-- Main JS -->
-    <script src="../assets/js/main.js"></script>
+    <script src="../../assets/js/main.js"></script>
   </body>
 </html>
