d411d6c9475de40cf304d55fcd51e5f34453dfe2
sleepy-head-r <rayanwisesaazhari@gmail.com>
2025-03-18 22:51:17 +0700

    Improve form validation pada halaman login perusahaan dan pelamar.

--- auth/pelamar/login.php
+++ auth/pelamar/login.php
@@ -5,28 +5,29 @@
     <!-- Logo -->
     <div class="app-brand justify-content-center">
       <a href="index.html" class="app-brand-link gap-2">
-        <span class="app-brand-text demo fw-bolder">KitaKerja</span>
+        <span class="app-brand-text demo fw-bolder">KitaKerja Jobs</span>
       </a>
     </div>
     <!-- /Logo -->
     <h4 class="mb-2">Selamat datang di kitakerja</h4>
-    <form class="mb-3" action="login_auth.php" method="POST">
+    <form class="mb-3 needs-validation" novalidate action="login.php" method="POST">
       <div class="mb-3">
-        <label class="form-label">Email</label>
-        <input type="text" id="email" class="form-control" name="email"
+        <label for="validationCustom01" class="form-label">Email</label>
+        <input id="validationCustom01" type="text" class="form-control" name="email"
           placeholder="Masukkan email anda" autofocus required />
-      </div>
-      <div class="mb-3 form-password-toggle">
-        <div class="d-flex justify-content-between">
-          <label class="form-label" for="password">Password</label>
-        </div>
-        <div class="input-group input-group-merge">
-          <input type="password" id="password" class="form-control" name="password"
-            placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
-            aria-describedby="password" />
-          <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
+        <div class="invalid-feedback">
+          Masukkan email anda.
         </div>
       </div>
+      <div class="mb-3">
+        <label for="validationCustom02" class="form-label">Password</label>
+        <input id="validationCustom02" type="password" class="form-control" name="password"
+          placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;" required/>
+        <div class="invalid-feedback">
+          Masukkan password anda.
+        </div>
+      </div>
+      
       <div class="mb-3">
         <button id="submit" class="btn btn-dark d-grid w-100" type="submit">Masuk</button>
       </div>
@@ -36,44 +37,28 @@
     </p>
   </div>
 </div>
+
 <script>
-  let submitBtn = document.getElementById('submit');
-  let formEmail = document.getElementById('email');
-  let formPassword = document.getElementById('password');
+  (function() {
+    'use strict'
 
-  let formAll = (formEmail, formPassword);
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
 
-  formAll.addEventListener("change", buttonenabler);
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function(form) {
+        form.addEventListener('submit', function(event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
 
-  submitBtn.setAttribute("disabled", true);
-
-  let isValid = true;
-function buttonenabler(){
-
-    if (formEmail == "") {
-        isValid = false;
-    } else {
-      isValid = true;
-    }
-
-    if (formPassword == "") {
-        isValid = false;
-    } else {
-      isValid = true;
-    }
-    
-    if (isValid === true) {
-      submitBtn.removeAttribute("disabled");
-    }
-
-    if (isValid === false){
-      submitBtn.setAttribute("disabled", true);
-    }
-
-}
-  
-        
-
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
 </script>
+
 <!-- /Register -->
 <?php include(".layouts/footer.php"); ?>
